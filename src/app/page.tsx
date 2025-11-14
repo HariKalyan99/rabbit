a435241c046cd266d0a4a9a140f2fd09e782add9
Harikalyan <harikalyan99tronics@gmail.com>
2025-11-14 16:38:20 +0530

    trpc database access layer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
-import { Button } from '@/components/ui/button'
-import prisma from '@/lib/db';
-import React from 'react'
+import { getQueryClient, trpc } from "@/trpc/server";
+import React, { Suspense } from "react";
+import Client from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-const page = async() => {
+const page = async () => {
+  const queryClient = getQueryClient();
 
-  const users = await prisma.user.findMany();
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
 
   return (
-    <div className='min-h-screen flex items-center justify-center bg-gray-300'>
-      {users.map((user) => <Button variant={"outline"}>
-        {user.name}
-      </Button>)}
+    <div className="min-h-screen flex items-center justify-center bg-gray-300">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
