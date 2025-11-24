1b79947818f85dc31346fa7dd1739e6fb1bae2a2
Harikalyan99 <harikalyan99tronics@gmail.com>
2025-11-24 13:28:00 +0530

    4:26 error tracking and sentry setup

--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -6,12 +6,20 @@ import { generateText } from "ai";
 
 const google = createGoogleGenerativeAI();
 const openAi = createOpenAI();
+import * as Sentry from "@sentry/nextjs";
 
 export const executeAi = inngest.createFunction(
   { id: "execute-ai" },
   { event: "execute/ai" },
   async ({ event, step }) => {
     await step.sleep("pretend", "5s");
+
+    console.warn("SOmething is missing");
+    console.error("added a error to track");
+    Sentry.logger.info("User triggered test log", {
+      log_source: "sentry_test",
+    });
+
     const { steps: geminiSteps } = await step.ai.wrap(
       "gemini-generate-text",
       generateText,
@@ -19,6 +27,11 @@ export const executeAi = inngest.createFunction(
         model: google("gemini-2.5-flash"),
         system: "You are a helpful assistant.",
         prompt: "What is 2 + 2",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       }
     );
 
@@ -29,6 +42,11 @@ export const executeAi = inngest.createFunction(
         model: openai("gpt-4"),
         system: "You are a helpful assistant.",
         prompt: "What is 2 + 2",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       }
     );
 
