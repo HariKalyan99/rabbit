a030157caeae198b91a44be1f1e758f627797065
Harikalyan99 <harikalyan99tronics@gmail.com>
2025-12-07 19:21:25 +0530

    30:41, execute workflow in background, partial commit

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -17,12 +17,14 @@ import {
   Panel,
 } from "@xyflow/react";
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import "@xyflow/react/dist/style.css";
 import { nodeComponents } from "@/config/node-components";
 import { AddNodeButton } from "./add-node-button";
 import { useSetAtom } from "jotai";
 import { editorAtom } from "../store/atoms";
+import { NodeType } from "@/generated/prisma/enums";
+import { ExecuteWorkflowButton } from "./execute-workflow-button";
 
 export const EditorLoading = () => {
   return <LoadingView message="Loading Editor..." />;
@@ -56,6 +58,10 @@ export const Editor = ({ workflowId }: { workflowId: string }) => {
     []
   );
 
+  const hasManualTrigger = useMemo(() => {
+    return nodes.some((node) => node.type === NodeType.MANUAL_TRIGGER);
+  }, [nodes]);
+
   return (
     <div className="size-full">
       <ReactFlow
@@ -82,6 +88,12 @@ export const Editor = ({ workflowId }: { workflowId: string }) => {
         <Panel position="top-right">
           <AddNodeButton />
         </Panel>
+
+        {hasManualTrigger && (
+          <Panel position="bottom-center">
+            <ExecuteWorkflowButton workflowId={workflowId} />
+          </Panel>
+        )}
       </ReactFlow>
     </div>
   );
