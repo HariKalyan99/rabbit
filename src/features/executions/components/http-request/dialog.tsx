e5fadb9ad6ca1236950e777dea0b843f34a48758
Harikalyan <harikalyan99tronics@gmail.com>
2025-12-07 13:02:32 +0530

    added the http request dialog form

--- src/features/executions/components/http-request/dialog.tsx
+++ src/features/executions/components/http-request/dialog.tsx
@@ -6,14 +6,83 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
+  DialogFooter,
 } from "@/components/ui/dialog";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
 
+const formSchema = z.object({
+  endpoint: z.url({ message: "Please enter a valid url" }),
+  method: z.enum(["GET", "POST", "PUT", "PATCH", "DELETE"]),
+  body: z.string().optional(),
+  // refine: json5
+});
+
+export type FormType = z.infer<typeof formSchema>;
 interface Props {
   open: boolean;
   onOpenChange: (open: boolean) => void;
+  onSubmit: (values: z.infer<typeof formSchema>) => void;
+  defaultEndpoint?: string;
+  defaultMethod?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+  defaultBody?: string;
 }
 
-export const HttpRequestDialog = ({ open, onOpenChange }: Props) => {
+export const HttpRequestDialog = ({
+  open,
+  onOpenChange,
+  onSubmit,
+  defaultBody = "",
+  defaultEndpoint = "",
+  defaultMethod = "GET",
+}: Props) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      endpoint: defaultEndpoint,
+      method: defaultMethod,
+      body: defaultBody,
+    },
+  });
+
+  useEffect(() => {
+    if (open) {
+      form.reset({
+        endpoint: defaultEndpoint,
+        method: defaultMethod,
+        body: defaultBody,
+      });
+    }
+  }, [open, defaultBody, defaultEndpoint, defaultMethod, form]);
+
+  const watchMethod = form.watch("method");
+  const showBodyField = ["POST", "PUT", "PATCH"].includes(watchMethod);
+  const handleSubmit = (values: z.infer<typeof formSchema>) => {
+    onSubmit(values);
+    onOpenChange(false);
+  };
+
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent>
@@ -23,6 +92,91 @@ export const HttpRequestDialog = ({ open, onOpenChange }: Props) => {
             Configure settings for the Http request node.
           </DialogDescription>
         </DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="space-y-8 mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="method"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Method</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Select a method" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="GET">GET</SelectItem>
+                      <SelectItem value="POST">POST</SelectItem>
+                      <SelectItem value="PUT">PUT</SelectItem>
+                      <SelectItem value="PATCH">PATCH</SelectItem>
+                      <SelectItem value="DELETE">DELETE</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormDescription>
+                    The method to use for the request.
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="endpoint"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Endpoint URL</FormLabel>
+                  <Input
+                    {...field}
+                    placeholder="https://api.example.com/users/{{httpResponse.data.id}}"
+                  />
+                  <FormDescription>
+                    Static URL or use {"{{variables}}"} for simple values or{" "}
+                    {"{{json variable}}"} to stringify objects.
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+            {showBodyField && (
+              <FormField
+                control={form.control}
+                name="body"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Request Body</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        {...field}
+                        placeholder={`{
+ "user_id": "{{httpResponse.data.id}}",
+ "name": "{{httpResponse.data.name}}",
+ "items": [{"id": "{{httpResponse.data.items.id}}"
+}`}
+                        className="min-h-[120px] font-mono text-sm"
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      JSON with tenplate variables. Use {"{{variables}}"} for
+                      simple values or {"{{json variable}}"} to stringify
+                      objects
+                    </FormDescription>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
+
+            <DialogFooter className="mt-4">
+              <Button type="submit">Save</Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
